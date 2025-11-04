446779625721efbcf6466844fe6589ab9bba0eb4
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-11-04 22:08:34 +0600

    setup bubble tea for the project

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	msg string
+}
+
+func (m model) Init() tea.Cmd {
+    // Just return `nil`, which means "no I/O right now, please."
+    return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+
+    // Is it a key press?
+    case tea.KeyMsg:
+
+        // Cool, what was the actual key pressed?
+        switch msg.String() {
+
+        // These keys should exit the program.
+        case "ctrl+c", "q":
+            return m, tea.Quit
+        }
+    }
+
+    return m, nil
+}
+
+func (m model) View() string {
+	return m.msg
+}
+
+func initializeModel() model {
+	return model{
+		msg: "TermNote - A terminal note app!",
+	}
+}
 func main() {
-	fmt.Println("Welcome to TermNote!")
+	p := tea.NewProgram(initializeModel())
+	if _, err := p.Run(); err != nil {
+        fmt.Printf("Alas, there's been an error: %v", err)
+        os.Exit(1)
+    }
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 build:
 	@go build -o termnote .
 
-run:
+run: build
 	@./termnote
