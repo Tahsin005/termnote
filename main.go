f2fa1ac2a9ee5a6ce772e59f4784ac129a1ad2a5
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-11-04 23:02:57 +0600

    add layout and ui using lipgloss

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
@@ -35,7 +36,25 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return m.msg
+	titleStyle := lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("#FAFAFA")).
+		Background(lipgloss.Color("#7D56F4")).
+		PaddingLeft(2).
+		PaddingRight(2)
+
+	sectionStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#D6D6D6"))
+
+	helpStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#999999")).
+		MarginTop(1)
+
+	welcome := titleStyle.Render("Welcome to TermNote! >_")
+	view := sectionStyle.Render("View coming soon...")
+	help := helpStyle.Render("Ctrl+N: new file . Ctrl+L: list . Esc: back/save . Ctrl+S: save . Ctrl+Q: quit")
+
+	return fmt.Sprintf("%s\n\n%s\n\n%s", welcome, view, help)
 }
 
 func initializeModel() model {
