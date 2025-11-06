6aac78cf3bb753ee274036159e75ad2f37bb62cb
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-11-06 11:13:44 +0600

    update  main.go with error handling and new file input

--- main.go
+++ main.go
@@ -2,37 +2,79 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
+var (
+	cursorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#7D56F4"))
+	vaultDir string
+)
+
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Error gettting home directory", err)
+	}
+	vaultDir = fmt.Sprintf("%s/.termnote", homeDir)
+}
+
 type model struct {
-	msg string
+	newFileInput           textinput.Model
+	createFileInputVisible bool
+	currentFile            *os.File
 }
 
 func (m model) Init() tea.Cmd {
-    // Just return `nil`, which means "no I/O right now, please."
-    return nil
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    switch msg := msg.(type) {
+	var cmd tea.Cmd
 
-    // Is it a key press?
-    case tea.KeyMsg:
+	switch msg := msg.(type) {
 
-        // Cool, what was the actual key pressed?
-        switch msg.String() {
+	case tea.KeyMsg:
 
-        // These keys should exit the program.
-        case "ctrl+c", "q":
-            return m, tea.Quit
-        }
-    }
+		switch msg.String() {
 
-    return m, nil
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "ctrl+n":
+			m.createFileInputVisible = true
+			return m, nil
+		case "enter":
+			fileName := m.newFileInput.Value()
+			if fileName != "" {
+				filePath := fmt.Sprintf("%s/%s.md", vaultDir, fileName)
+
+				if _, err := os.Stat(filePath); err == nil {
+					return m, nil
+				}
+
+				f, err := os.Create(filePath)
+				if err != nil {
+					log.Fatal("Error creating file", err)
+				}
+
+				m.currentFile = f
+				m.createFileInputVisible = false
+				m.newFileInput.SetValue("")
+			}
+			return m, nil
+		}
+	}
+
+	if m.createFileInputVisible {
+		m.newFileInput, cmd = m.newFileInput.Update(msg)
+	}
+
+	return m, cmd
 }
 
 func (m model) View() string {
@@ -43,29 +85,44 @@ func (m model) View() string {
 		PaddingLeft(2).
 		PaddingRight(2)
 
-	sectionStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#D6D6D6"))
-
 	helpStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#999999")).
-		MarginTop(1)
+		Foreground(lipgloss.Color("#999999"))
 
 	welcome := titleStyle.Render("Welcome to TermNote! >_")
-	view := sectionStyle.Render("View coming soon...")
-	help := helpStyle.Render("Ctrl+N: new file . Ctrl+L: list . Esc: back/save . Ctrl+S: save . Ctrl+Q: quit")
+	help := helpStyle.Render("Ctrl+n: new file . Ctrl+l: list . Esc: back/save . Ctrl+s: save . Ctrl+q: quit")
+
+	view := ""
+	if m.createFileInputVisible {
+		view = m.newFileInput.View()
+	}
 
 	return fmt.Sprintf("%s\n\n%s\n\n%s", welcome, view, help)
 }
 
 func initializeModel() model {
+	err := os.MkdirAll(vaultDir, 0750)
+	if err != nil {
+		log.Fatal("Error creating vault directory", err)
+	}
+
+	// initialize new file input
+	ti := textinput.New()
+	ti.Placeholder = "Enter the name of your new note..."
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 50
+	ti.Cursor.Style = cursorStyle
+	ti.PromptStyle = cursorStyle
+
 	return model{
-		msg: "TermNote - A terminal note app!",
+		newFileInput:           ti,
+		createFileInputVisible: false,
 	}
 }
 func main() {
 	p := tea.NewProgram(initializeModel())
 	if _, err := p.Run(); err != nil {
-        fmt.Printf("Alas, there's been an error: %v", err)
-        os.Exit(1)
-    }
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
